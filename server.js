f36f737ad189572625f23d57c13772433b5f1d45
ahmad420 <ahmdaf420@gmail.com>
2021-03-30 03:04:49 +0300

    routes added

--- server.js
+++ server.js
@@ -1,25 +1,19 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
-import products from './data/products.js'
 import connectDB from './config/db.js'
+
+import productRoutes from './routes/productRoutes.js'
 
 dotenv.config()
 connectDB()
 const server = express()
 
 server.get('/', (req, res) => {
-  res.send('<h1>hello express</h1>')
+  res.send('<h1>express is running ... </h1>')
 })
 
-server.get('/api/products', (req, res) => {
-  res.json(products)
-})
-
-server.get('/api/products/:id', (req, res) => {
-  const product = products.find((p) => p._id === req.params.id)
-  res.json(product)
-})
+server.use('/api/products', productRoutes)
 
 const PORT = process.env.PORT || 5000
 server.listen(PORT, () =>
